4d66464a7f372c6750e239820ebfe11be0c5af61
Sang Huynh <sang.huynh@innogram.co>
2022-09-10 11:05:15 +0700

    [SA-6] Fix rubocop

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Attachment, type: :model do
-  describe "associations" do
+  describe 'associations' do
     it { should belong_to(:user) }
   end
 
-  describe "validations" do
+  describe 'validations' do
     it { should validate_presence_of(:content) }
   end
 end
