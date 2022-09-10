11e104fc3aeb46cace6c738f539a3d0656b91c97
Sang Huynh <sang.huynh@innogram.co>
2022-09-10 10:38:05 +0700

    [SA-6] Migration create Attachment

---
+++ db/migrate/20220910033331_create_attachments.rb
@@ -0,0 +1,10 @@
+class CreateAttachments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :attachments do |t|
+      t.string :content, null: false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_09_234337) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_10_033331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attachments", force: :cascade do |t|
+    t.string "content", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_attachments_on_user_id"
+  end
+
+  create_table "files", force: :cascade do |t|
+    t.string "content"
+    t.bigint "files_id"
+    t.bigint "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["files_id"], name: "index_files_on_files_id"
+    t.index ["users_id"], name: "index_files_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_09_234337) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "attachments", "users"
 end
