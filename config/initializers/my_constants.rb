51e3632398538bd3842715e5a143c0183c38f057
Sang Huynh <sang.huynh@innogram.co>
2022-09-12 23:19:14 +0700

    [SA-8] Update constant

--- config/initializers/my_constants.rb
+++ config/initializers/my_constants.rb
@@ -1,2 +1,7 @@
 CSV_FILE_TYPE = 'text/csv'.freeze
 JSON_FILE_TYPE = 'application/json'.freeze
+GOOGLE_SEARCH_URI = 'https://www.google.com/search?q='.freeze
+HEADER_AUTHORITY = 'www.google.com'.freeze
+HEADER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'.freeze
+ADS_ID = '#tads'.freeze
+TOTAL_RESULTS_ID = '#result-stats'.freeze
