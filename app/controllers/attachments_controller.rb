6c26639b7a391e45cd1e0b4935f15a4604dae2ce
Sang Huynh <sang.huynh@innogram.co>
2022-09-13 09:32:11 +0700

    [SA-11] Add action and href to list attachments

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,4 +1,15 @@
 class AttachmentsController < ApplicationController
+  def index
+    last_attachment = Attachment.last
+
+    redirect_to results_attachment_path(last_attachment)
+  end
+
+  def results
+    @attachments = Attachment.where(user_id: current_user.id)
+    @attachment = Attachment.includes(:results).find_by(id: params[:id], user_id: current_user.id)
+  end
+
   def create
     result = Attachments::CreateService.new(file: attachment_params[:file]).call
 
