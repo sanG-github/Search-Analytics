ae2f4520b9ddb88dc10456029cd6f596a416ce66
Sang Huynh <sang.huynh@innogram.co>
2022-09-14 21:43:40 +0700

    [SA-15] Add toast for realtime notification

--- app/models/result.rb
+++ app/models/result.rb
@@ -2,5 +2,7 @@ class Result < ApplicationRecord
   belongs_to :attachment
   has_one :source_code
 
+  delegate :user, to: :attachment
+
   enum status: { fetching: 1, done: 2 }
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
+pin "jquery", to: "jquery.min.js", preload: true
+pin "jquery_ujs", to: "jquery_ujs.js", preload: true
 pin 'application', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
@@ -7,3 +9,4 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin '@rails/actioncable', to: 'actioncable.esm.js'
 pin_all_from 'app/javascript/channels', under: 'channels'
+pin 'toastr', to: 'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js'
