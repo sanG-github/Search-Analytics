3fc5a08b55927b3cb1d1077f8c4bcc193d1a4f19
Sang Huynh <sang.huynh@innogram.co>
2022-09-15 23:42:05 +0700

    [SA-19] Implement search by keyword

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,7 @@
 class ResultsController < ApplicationController
-  def index; end
+  def index
+    @results = current_user.results.where('keyword LIKE ?', "%" + params[:q] + "%")
+  end
 
   def show
     @result = current_user.results.find_by(id: params[:id])
