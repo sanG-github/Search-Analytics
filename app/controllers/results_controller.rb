554d7ab0b3f798abc2837ed46d4fe3bd657b3a90
Sang Huynh <sang.huynh@innogram.co>
2022-09-20 13:49:34 +0700

    Using `srcdoc` instead of `src` (iframe)

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,21 +1,13 @@
 class ResultsController < ApplicationController
-  before_action :set_result, only: %i[show preview]
-
   def index
     @results = current_user.results.by_keyword(params[:q].to_s)
   end
 
-  def show; end
-
-  def preview
-    render html: @result.source_code.content.html_safe
+  def show
+    @result = current_user.results.find_by(id: params[:id])
   end
 
   private
-
-  def set_result
-    @result = current_user.results.find_by(id: params[:id])
-  end
 
   def attachment_params
     params.require(:attachment).permit(:file)
