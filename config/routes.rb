4ca68cda641094168e226a673374b2f25802da7a
Sang Huynh <sang.huynh@innogram.co>
2022-09-20 11:11:15 +0700

    Add preview page for google result

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
   root to: 'home#index'
-  resources :results, only: %i[index show]
+  resources :results, only: %i[index show] do
+    get :preview, on: :member
+  end
   resources :attachments, only: %i[index create] do
     get :results, on: :member
   end
+
 
   draw(:external_apis)
 end
