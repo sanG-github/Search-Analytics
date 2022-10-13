f6957d692ccfbe6611d9415fdc1e1e56c12e5baf
Sang Huynh <sanghuynh20000@gmail.com>
2022-10-13 15:47:02 +0700

    Fix some codes that follow convention

--- spec/controllers/results_controller_spec.rb
+++ spec/controllers/results_controller_spec.rb
@@ -10,38 +10,41 @@ RSpec.describe ResultsController, type: :controller do
   end
 
   describe 'GET #index' do
-    subject { get :index }
-
-    let(:number_of_results) { 10 }
-    let(:attachment) { create :attachment, user_id: user.id }
-    let(:un_authored_attachment) { create :attachment, user_id: other_user.id }
-
     context 'without params' do
-      before { create_list :result, number_of_results, attachment_id: attachment.id }
-
       it 'returns all results of user' do
-        expect(subject).to have_http_status :success
-        expect(controller.instance_variable_get(:@results).size).to eq(number_of_results)
-        expect(controller.instance_variable_get(:@results).klass.name).to eq(Result.name)
-        expect(controller.instance_variable_get(:@results).pluck('DISTINCT attachment_id')).not_to include(un_authored_attachment.id)
+        number_of_results = 10
+        attachment = create :attachment, user_id: user.id
+        un_authored_attachment = create :attachment, user_id: other_user.id
+        create_list :result, number_of_results, attachment_id: attachment.id
+
+        get :index
+        results = controller.instance_variable_get(:@results)
+
+        expect(response).to have_http_status :success
+        expect(results.size).to eq(number_of_results)
+        expect(results.klass.name).to eq(Result.name)
+        expect(results.pluck('DISTINCT attachment_id')).not_to include(un_authored_attachment.id)
       end
     end
 
-    context 'filtered by keyword' do
-      subject { get :index, params: { q: keyword } }
-
-      let(:keyword) { 'e' }
-      let!(:nimble_result) { create :result, keyword: 'Nimble', attachment_id: attachment.id }
-      let!(:company_result) { create :result, keyword: 'company', attachment_id: attachment.id }
-      let!(:vietnam_result) { create :result, keyword: 'VietNam', attachment_id: attachment.id }
-
+    context 'when filtered by keyword' do
       it 'returns all filtered results of user' do
-        expect(subject).to have_http_status :success
-        expect(controller.instance_variable_get(:@results).size).to eq(2)
-        expect(controller.instance_variable_get(:@results).klass.name).to eq(Result.name)
-        expect(controller.instance_variable_get(:@results).pluck('DISTINCT keyword')).to include(nimble_result.keyword, vietnam_result.keyword)
-        expect(controller.instance_variable_get(:@results).pluck('DISTINCT keyword')).not_to include(company_result.keyword)
-        expect(controller.instance_variable_get(:@results).pluck('DISTINCT attachment_id')).not_to include(un_authored_attachment.id)
+        keyword = 'e'
+        attachment = create :attachment, user_id: user.id
+        nimble_result = create :result, keyword: 'Nimble', attachment_id: attachment.id
+        company_result = create :result, keyword: 'company', attachment_id: attachment.id
+        vietnam_result = create :result, keyword: 'VietNam', attachment_id: attachment.id
+        un_authored_attachment = create :attachment, user_id: other_user.id
+
+        get :index, params: { q: keyword }
+        results = controller.instance_variable_get(:@results)
+
+        expect(response).to have_http_status :success
+        expect(results.size).to eq(2)
+        expect(results.klass.name).to eq(Result.name)
+        expect(results.pluck('DISTINCT keyword')).to include(nimble_result.keyword, vietnam_result.keyword)
+        expect(results.pluck('DISTINCT keyword')).not_to include(company_result.keyword)
+        expect(results.pluck('DISTINCT attachment_id')).not_to include(un_authored_attachment.id)
       end
     end
   end
@@ -55,7 +58,10 @@ RSpec.describe ResultsController, type: :controller do
     let(:un_authored_result) { create :result, attachment_id: un_authored_attachment.id }
 
     it 'returns result of user' do
+      attachment = create :attachment, user_id: user.id
+      result = create :result, attachment_id: attachment.id
       params = { id: result.id }
+
       get :show, params: params
 
       expect(response).to have_http_status :success
@@ -64,7 +70,10 @@ RSpec.describe ResultsController, type: :controller do
     end
 
     it 'returns nil for un-authored result' do
+      un_authored_attachment = create :attachment, user_id: other_user.id
+      un_authored_result = create :result, attachment_id: un_authored_attachment.id
       params = { id: un_authored_result.id }
+
       get :show, params: params
 
       expect(response).to have_http_status :success
