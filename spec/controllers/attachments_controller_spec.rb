fb8828ade9ab0d34743debbc72edf618c039aea9
Sang Huynh <sanghuynh20000@gmail.com>
2022-10-13 17:00:09 +0700

    Fix some codes that follow convention

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -9,19 +9,21 @@ RSpec.describe AttachmentsController, type: :controller do
   end
 
   describe 'GET #index' do
-    subject { get :index }
-
-    context 'has at least one attachment' do
-      let(:last_attachment) { user.attachments.last }
-      before { create_list :attachment, 10, user_id: user.id }
-
+    context 'when the user has at least one attachment' do
       it 'redirect to the last attachment detail' do
-        expect(subject).to redirect_to results_attachment_path(last_attachment)
+        create_list :attachment, 10, user_id: user.id
+        last_attachment = user.attachments.last
+
+        get :index
+
+        expect(response).to redirect_to results_attachment_path(last_attachment)
       end
     end
 
     context 'not have any attachments' do
       it 'renders the page with an instantiated instance variable' do
+        get :index
+
         expect(subject).to render_template(:index)
         expect(assigns(:attachment)).to be_a(Attachment)
         expect(assigns(:attachment)).to be_new_record
@@ -30,44 +32,43 @@ RSpec.describe AttachmentsController, type: :controller do
   end
 
   describe 'GET #results' do
-    subject { get :results, params: { id: attachment.id } }
-
-    let(:attachment) { attachments.sample }
-    let(:attachments) { create_list :attachment, 10, user_id: user.id }
-
     it 'renders the page with instance variables' do
-      expect(subject).to render_template(:results)
+      attachments = create_list :attachment, 10, user_id: user.id
+      attachment = attachments.sample
+
+      get :results, params: { id: attachment.id }
+
+      expect(response).to render_template(:results)
       expect(assigns(:attachments)).to eq user.attachments.order(created_at: :desc)
       expect(assigns(:attachment)).to eq attachment
     end
   end
 
   describe 'POST #create' do
-    subject { post :create, params: params }
-
-    let(:attachment) { build :attachment }
-    let(:params) do
-      {
-        attachment: {
-          file: file
-        }
-      }
-    end
-
     context 'with valid file' do
-      let(:file) { fixture_file_upload('valid_file.csv', 'text/csv') }
-
       it 'create a new attachment and redirect the the detail page' do
-        expect{ subject }.to change{ user.reload.attachments.size }.from(0).to(1)
-        expect(subject).to redirect_to results_attachment_path(user.reload.attachments.last)
+        params = {
+          attachment: {
+            file: fixture_file_upload('valid_file.csv', 'text/csv')
+          }
+        }
+        post :create, params: params
+
+        expect(user.attachments.size).to eq(1)
+        expect(response).to redirect_to results_attachment_path(user.reload.attachments.last)
       end
     end
 
     context 'with invalid file' do
-      let(:file) { fixture_file_upload('empty_file.csv', 'text/csv') }
-
       it 'send error message by worker' do
-        expect{ subject }.to change(PushNotificationWorker.jobs, :size).by(1)
+        params = {
+          attachment: {
+            file: fixture_file_upload('empty_file.csv', 'text/csv')
+          }
+        }
+        post :create, params: params
+
+        expect(PushNotificationWorker.jobs.size).to eq(1)
       end
     end
   end
