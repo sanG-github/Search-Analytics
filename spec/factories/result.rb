03fa57a99c21f9e0a732376a66176f9418761987
Sang Huynh <sanghuynh20000@gmail.com>
2022-10-19 16:16:57 +0700

    Change factories data to using FFaker instead of Faker

--- spec/factories/result.rb
+++ spec/factories/result.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :result do
     attachment
 
-    total_ads { Faker::Number.number(digits: 2) }
-    total_links { Faker::Number.number(digits: 2) }
-    total_results { Faker::Number.number(digits: 2) }
+    total_ads { FFaker::Number.number(digits: 2) }
+    total_links { FFaker::Number.number(digits: 2) }
+    total_results { FFaker::Number.number(digits: 2) }
     status { Result.statuses.keys.sample }
-    keyword { Faker::Name.name }
+    keyword { FFaker::Name.name }
   end
 end
