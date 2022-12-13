ae56c0c4978d5ebcedb2478be066746ba0db5a91
Sang Huynh <sanghuynh20000@gmail.com>
2022-12-13 12:15:58 +0700

    Update test for index action

--- spec/controllers/results_controller_spec.rb
+++ spec/controllers/results_controller_spec.rb
@@ -18,20 +18,19 @@ RSpec.describe ResultsController, type: :controller do
 
       it 'returns all results of the user' do
         user = create :user
-        another_user = create :user
         number_of_results = 10
         attachment = create :attachment, user_id: user.id
+        user_results = create_list :result, number_of_results, attachment_id: attachment.id
+
+        another_user = create :user
         another_user_attachment = create :attachment, user_id: another_user.id
-        create_list :result, number_of_results, attachment_id: attachment.id
+        create_list :result, number_of_results, attachment_id: another_user_attachment.id
 
         sign_in user
         get :index
         results = controller.instance_variable_get(:@results)
 
-        expect(response).to have_http_status :success
-        expect(results.size).to eq(number_of_results)
-        expect(results.klass.name).to eq(Result.name)
-        expect(results.pluck('DISTINCT attachment_id')).not_to include(another_user_attachment.id)
+        expect(results).to contain_exactly(*user_results)
       end
     end
 
